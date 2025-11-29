ed035e283a69b005eb057680a68a970de9f87811
Jonan <jjsalomonzon@gmail.com>
2025-11-29 14:15:56 -0800

    Add asset loading utility and remove PartyWidgetUIEditor
    Introduced a generic LoadAsset utility and reinitialization of GameService systems in GURUServiceEditor. Removed PartyWidgetUIEditor and its meta file, likely as part of code cleanup or deprecation.

--- Assets/Editor/GURUServiceEditor.cs
+++ Assets/Editor/GURUServiceEditor.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 [CustomEditor(typeof(GURUService), true)]
 public class GURUServiceEditor : Editor
 {
+    private static T LoadAsset<T>(string typeName) where T : UnityEngine.Object
+    {
+        string[] guids = AssetDatabase.FindAssets("t:" + typeName);
+        if (guids.Length > 0)
+        {
+            string path = AssetDatabase.GUIDToAssetPath(guids[0]);
+            return AssetDatabase.LoadAssetAtPath<T>(path);
+        }
+        return null;
+    }
+
     public override void OnInspectorGUI()
     {
         GURUService service = (GURUService)target;
+
+        // Reinitialize systems
+        GameService gs = LoadAsset<GameService>("GameService");
+        if (gs != null) gs.InitializeSystems();
 
         DrawDefaultInspector();
 
