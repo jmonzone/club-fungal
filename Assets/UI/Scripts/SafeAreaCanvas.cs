cd181975ed24ef861ddcc6ceb1723b0d36492714
Jonan <jjsalomonzon@gmail.com>
2025-11-28 18:35:08 -0800

    added safe area

--- Assets/UI/Scripts/SafeAreaCanvas.cs
+++ Assets/UI/Scripts/SafeAreaCanvas.cs
@@ -1,22 +1,52 @@
 using UnityEngine;
 
 [RequireComponent(typeof(RectTransform))]
+[ExecuteInEditMode]
 public class SafeAreaCanvas : MonoBehaviour
 {
     [SerializeField] private RectTransform rectTransform;
     [SerializeField] private Canvas canvas;
+    [SerializeField] private float uniformPaddingPixels = 0;
 
-    private void OnValidate()
+    private Rect lastSafeArea;
+
+    private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
         canvas = GetComponentInParent<Canvas>();
         ApplySafeArea();
+        lastSafeArea = Screen.safeArea;
     }
+
+    private void OnValidate()
+    {
+        ApplySafeArea();
+    }
+
+#if UNITY_EDITOR
+    private void Update()
+    {
+        if (Screen.safeArea != lastSafeArea)
+        {
+            ApplySafeArea();
+            lastSafeArea = Screen.safeArea;
+        }
+    }
+#endif
 
     public void ApplySafeArea()
     {
         Rect canvasRect = canvas.pixelRect;
         Rect safeArea = Screen.safeArea;
+
+        // Apply uniform padding
+        float padding = uniformPaddingPixels;
+        safeArea = new Rect(
+            safeArea.x + padding,
+            safeArea.y + padding,
+            Mathf.Max(0, safeArea.width - 2 * padding),
+            Mathf.Max(0, safeArea.height - 2 * padding)
+        );
 
         // Clip safe area to canvas rect
         safeArea = Rect.MinMaxRect(
